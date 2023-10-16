46a7834890b83ce51e0ff725fa2250c673ee5ac6
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-10-16 18:26:51 +0530

    labs and reference notes added

---
+++ Reference Notes and Labs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Reference_Notes_and_Labs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
