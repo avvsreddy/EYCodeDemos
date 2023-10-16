39f86db5ccda885e55bb667c52de0ef3b48e8ffa
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-10-16 18:09:47 +0530

    new layered code added

--- FristConsoleApp/Program.cs
+++ FristConsoleApp/Program.cs
@@ -1,12 +1,33 @@
 ﻿namespace FristConsoleApp
 {
-    internal class Program
+    internal class Program // UI
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  // UI // SRP
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Welcome to my first .net console application");
-            Console.ReadKey();
+            // Accept two numbers and find the max number then disply
+
+            // step 1: declare three int varialble
+            int fno, sno, max;
+
+            // step 2: accept two int numbers into variables
+            Console.Write("Enter First Number: ");
+            fno = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Second Number: ");
+            sno = int.Parse(Console.ReadLine());
+
+            // step 3: find the max
+            max = SimpleMathLibrary.SimpleMath.FindMax(fno, sno);
+
+            // step 4: display the result
+            Console.WriteLine($"The maximum of {fno} and {sno} is {max}");
+            //Console.WriteLine("The maximum of " + fno + " and " + sno + " is " + max);
+            //Console.WriteLine("The maximum of {0} and {1} is {2}", fno, sno, max);
+
+
         }
+
+
     }
 }
+
