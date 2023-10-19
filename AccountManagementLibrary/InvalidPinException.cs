76e79d4e14fc2d5793ebd6bdd5ed071644130d39
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-10-19 16:17:43 +0530

    unit testing demo

---
+++ AccountManagementLibrary/InvalidPinException.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountManagementLibrary
+{
+    public class InvalidPinException : ApplicationException
+    {
+        public InvalidPinException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
