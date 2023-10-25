0d877741d3f56b567103dfe06c8991e7e9dd2dc4
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-10-25 11:16:10 +0530

    delegates demo1

---
+++ DelegatesDemo1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DelegatesDemo1
+{
+
+    //class MyDelegate : Delegate
+    //{
+
+    //}
+
+    // Step 1: Delegate Declaration
+    delegate void MyDelegate(string str);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Delegates Demo
+            // Direct method call
+            //DelegatesDemo1.Program.Greetings("Hello");
+            // Indirect method call - delgates
+            // 1. Declare a delegate
+            // 2. instantiate a delete
+
+            // Delegate d = new  Delegate ();
+            MyDelegate d = new MyDelegate(Greetings);
+
+            // 3. initialize a delegate
+            // 4. invoke a delegate
+            //d.Invoke("hello");
+            d("Hello");
+        }
+
+        static void Greetings(string msg)
+        {
+            Console.WriteLine($"Greetings : {msg}");
+        }
+    }
+}
