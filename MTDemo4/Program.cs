80b52e0d893e45b146ab855aa188de05d64952ba
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-10-26 12:01:58 +0530

    .

---
+++ MTDemo4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MTDemo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigData data = new BigData();
+            //data.Fill();
+            //data.Fill();
+            Parallel.Invoke(data.Fill, data.Fill);
+            Console.WriteLine(data.stack.Count);
+        }
+    }
+
+    public class BigData
+    {
+        public Stack<int> stack = new Stack<int>();
+
+        public void Fill()
+        {
+            for (int i = 1; i <= 10000000; i++)
+            {
+                stack.Push(i);
+            }
+        }
+    }
+}
