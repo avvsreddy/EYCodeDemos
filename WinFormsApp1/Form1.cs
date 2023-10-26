be47c47ef04bcaf04223a402749cf4017d48ef35
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-10-26 11:43:42 +0530

    .

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -9,41 +9,47 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // draw red rectangles
-            Graphics red = panel1.CreateGraphics();
-            Random rnd = new Random();
 
-
-
-            for (int i = 1; i <= 1000; i++)
+            new Thread(() =>
             {
-                int x = rnd.Next(panel1.Height);
-                int y = rnd.Next(panel1.Width);
+                // draw red rectangles
+                Graphics red = panel1.CreateGraphics();
+                Random rnd = new Random();
 
-                red.DrawRectangle(Pens.Red, x, y, 40, 40);
-                Thread.Sleep(100);
-            }
 
+
+                for (int i = 1; i <= 1000; i++)
+                {
+                    int x = rnd.Next(panel1.Height);
+                    int y = rnd.Next(panel1.Width);
+
+                    red.DrawRectangle(Pens.Red, x, y, 40, 40);
+                    Thread.Sleep(100);
+                }
+            }).Start();
 
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // draw blue rectangles
-            Graphics blue = panel2.CreateGraphics();
-            Random rnd = new Random();
-
-
-
-            for (int i = 1; i <= 1000; i++)
+            new Task(() =>
             {
-                int x = rnd.Next(panel2.Height);
-                int y = rnd.Next(panel2.Width);
+                // draw blue rectangles
+                Graphics blue = panel2.CreateGraphics();
+                Random rnd = new Random();
 
-                blue.DrawRectangle(Pens.Blue, x, y, 40, 40);
-                Thread.Sleep(100);
-            }
+
+
+                for (int i = 1; i <= 1000; i++)
+                {
+                    int x = rnd.Next(panel2.Height);
+                    int y = rnd.Next(panel2.Width);
+
+                    blue.DrawRectangle(Pens.Blue, x, y, 40, 40);
+                    Thread.Sleep(100);
+                }
+            }).Start();
         }
     }
 }
