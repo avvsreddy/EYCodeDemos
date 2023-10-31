6901da7393af2e5040196682592a49edbf791f98
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-10-31 15:52:55 +0530

    linq demos, labs, ref

---
+++ LineDemo7/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace LineDemo7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 14, 25, 3, 42, 5, 62, 7, 80, 9 };
+
+            // get all evens in assendign order
+
+            // sql: select number from numbers where number mod 2 = 0 order by number
+
+            // linq expression
+            var result1 = from n in numbers
+                          where n % 2 == 0
+                          orderby n descending
+                          select n;
+
+            // linq statement - extension methods, lamba
+
+            Console.WriteLine("result 1");
+            foreach (var item in result1)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Func<int, bool> filter = new Func<int, bool>(IsEven);
+
+            var result2 = numbers
+                .Where(n => n % 2 == 0)
+                .OrderByDescending(i => i)
+                .Select(x => x);
+
+            Console.WriteLine("result 2");
+            foreach (var item in result2)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        public static bool IsEven(int n)
+        {
+            return n % 2 == 0;
+        }
+    }
+}
---
+++ LinqDemo8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace LinqDemo8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Extension method
+
+            string str = "Hello";
+            int a = 10;
+            //a.Encrypt();
+            str = str.ToLower();
+
+            str = str.Encrypt();
+
+            Program p = new Program();
+            //p.Encrypt();
+
+            //str = MyUtil.Encrypt(str);
+
+            List<int> list = new List<int>();
+
+            list.Where(x => true);
+
+
+            //Enumerable
+            //var result = list.A
+        }
+    }
+
+    public static class Enumerable1
+    {
+        public static string Encrypt(this IEnumerable str)
+        {
+            return "@#$@#$WEFERT@#%@#$@#$%@%@#%@$%";
+        }
+
+
+    }
+}
