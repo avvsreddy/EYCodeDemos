397c16d83462f5c82bff7a7210fa8417a5982a60
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-10-31 13:05:47 +0530

    linq demos

---
+++ LinqDemo5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Linq;
+
+namespace LinqDemo5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> words = new List<string>() { "one", "two", "three", "four", "five", "six" };
+
+            // load xml doc into mem
+            XDocument xml = XDocument.Load("XMLFile1.xml");
+
+            // get all short words = 3 or less char
+
+            //var shortWords = from w in words
+            //                 where w.Length <= 3
+            //                 select w;
+
+            var shortWords = from w in xml.Descendants("word")
+                             where w.Value.Length <= 3
+                             select w.Value;
+
+            foreach (var item in shortWords)
+            {
+                Console.WriteLine(item);
+            }
+
+            // load xml doc
+
+        }
+    }
+}
---
+++ LinqToObjectsDemo1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace LinqToObjectsDemo1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // LINQ to Objects Demo
+
+
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            // extract all even numbers into another array and display
+            // by traditional approach - c/c++/java/????
+
+
+
+            // table: numbers
+            // column: number
+            // SQL: select number from numbers where number mod 2 = 0;
+
+            // var : local variable type inference
+            // LINQ Expression
+
+            var evenNumbers = from n in numbers
+                              where n % 2 == 0
+                              select n;
+
+
+
+
+            //List<int> evens = new List<int>();
+
+            //foreach (var number in numbers)
+            //{
+            //    if (number % 2 == 0)
+            //    {
+            //        evens.Add(number);
+            //    }
+            //}
+
+            foreach (var number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
